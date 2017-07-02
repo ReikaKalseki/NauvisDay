489050616f034ebb8f958aa7624b965c62a53321
Reika <reikasminecraft@gmail.com>
2017-07-02 01:49:44 -0400

    Update 1

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config = {}
 
-Config.basePollutionFactor = 1 --A multiplier for the pollution production. Multiplied against internal values. Increase this to make even MORE pollution.
+Config.basePollutionFactor = settings.startup["base-pollution-factor"].value--1 --A multiplier for the pollution production. Multiplied against internal values. Increase this to make even MORE pollution.
 
-Config.pollutedWaterThreshold = 10000 --the min amount of air pollution in a chunk before it will start contaminating the water
-Config.cleanWaterThreshold = 2000 --the max amount of air pollution in a chunk before contaminated water will start cleaning again; must be much (several times cleanup) less than the polluting threshold or you get water flipping back and forth
-Config.pollutedWaterTileCleanup = 100 --the amount of air pollution removed when a water tile turns polluted and, if cleaned, released when it turns back
+Config.pollutedWaterThreshold = settings.startup["polluted-water-threshold"].value--20000 --the min amount of air pollution in a chunk before it will start contaminating the water
+Config.cleanWaterThreshold = settings.startup["clean-water-threshold"].value--5000 --the max amount of air pollution in a chunk before contaminated water will start cleaning again; must be much (several times cleanup) less than the polluting threshold or you get water flipping back and forth
+Config.pollutedWaterTileCleanup = settings.startup["polluted-water-tile-cleanup"].value--100 --the amount of air pollution removed when a water tile turns polluted and, if cleaned, released when it turns back
---
+++ settings.lua
@@ -0,0 +1,38 @@
+data:extend({
+        {
+            type = "double-setting",
+            name = "base-pollution-factor",
+            setting_type = "startup",
+            default_value = 1,
+            order = "r",
+			localised_name = "Base pollution factor",
+			localised_description = "A (decimal) multiplier for the pollution production. Multiplied against internal values. Increase this to make even MORE pollution. Lower bound clamped at 1.0.",
+        },
+        {
+            type = "int-setting",
+            name = "polluted-water-threshold",
+            setting_type = "startup",
+            default_value = 20000,
+            order = "r",
+			localised_name = "Water pollution threshold",
+			localised_description = "The minimum amount of air pollution in a chunk before it will start contaminating the water",
+        },
+        {
+            type = "int-setting",
+            name = "clean-water-threshold",
+            setting_type = "startup",
+            default_value = 5000,
+            order = "r",
+			localised_name = "Water recovery threshold",
+			localised_description = "The maximum amount of air pollution in a chunk before contaminated water will start cleaning again; MUST be much (several times cleanup) less than the polluting threshold or you get water flipping back and forth",
+        },
+        {
+            type = "int-setting",
+            name = "polluted-water-tile-cleanup",
+            setting_type = "startup",
+            default_value = 100,
+            order = "r",
+			localised_name = "Polluted water tile cleanup",
+			localised_description = "The amount of air pollution removed when a water tile turns polluted and, if cleaned, released when it turns back.",
+        },
+})
